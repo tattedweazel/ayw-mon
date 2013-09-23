6288aace207ae7f25611861a7434eee705728918
tattedweazel <tattedweazel@gmail.com>
2013-09-22 23:06:25 -0700

    Minification script now working - command-line only

--- minifi.php
+++ minifi.php
@@ -1,15 +1,15 @@
 <?php
 
-echo "<br/><br/>";
+echo "\n\n";
 //$target = $argv[1];
-$target = '../assets/css';
+$target = 'assets/css';
 minifi($target);
-$target = '../assets/javascript';
+$target = 'assets/javascript';
 minifi($target);
 
 function minifi($target){
 	if ($target == '?'){
-		echo "<br/><br/>Just give me the path of the directory (relative to this file...) you want me to check<br/>and I'll skip through and minify all your css and javascript.<br/><br/>I will always minify any non .min.* files (as long as they're css or js files. I don't do that other<br/>werid stuff), but don't worry, the originals remain unscathed.<br/><br/>This means if you run me and I minify home.css, then you run me again on the same directory<br/>I will minify home.css again.<br/><br/>I will also always just save over any .min.* files that I make that may already exist.<br/>But you'd only be worried if you were editing minified files anyways.<br/>And besides... who does that?!<br/><br/>";
+		echo "\n\nJust give me the path of the directory (relative to this file...) you want me to check\nand I'll skip through and minify all your css and javascript.\n\nI will always minify any non .min.* files (as long as they're css or js files. I don't do that other\nwerid stuff), but don't worry, the originals remain unscathed.\n\nThis means if you run me and I minify home.css, then you run me again on the same directory\nI will minify home.css again.\n\nI will also always just save over any .min.* files that I make that may already exist.\nBut you'd only be worried if you were editing minified files anyways.\nAnd besides... who does that?!\n\n";
 		exit;
 	}
 	echo "Looking in ".$target;
@@ -18,7 +18,7 @@ function minifi($target){
 	$dir = opendir($target);
 
 	if (!$dir) {
-		echo "You have provided an invalid directory name<br/><br/>";
+		echo "You have provided an invalid directory name\n\n";
 		exit;
 	}
 
@@ -33,30 +33,30 @@ function minifi($target){
 		$ext = array_pop($filePieceArray);
 		$minCheck = array_pop($filePieceArray);
 		if (!$minCheck){
-			echo "<br/>File: ".$file." -> Not interested. Moving on.";
+			echo "\nFile: ".$file." -> Not interested. Moving on.";
 		}
 		elseif ($minCheck != 'min'){
 			switch ($ext){
 				case 'css':
-					echo "<br/>File: ".$file." -> Eligible CSS file found!!! Queueing for minification";
+					echo "\nFile: ".$file." -> Eligible CSS file found!!! Queueing for minification";
 					$css[] = $file;				
 					break;
 				case 'js':
-					echo "<br/>File: ".$file." -> Eligible JavaScript file found!!! Queueing for minification";
+					echo "\nFile: ".$file." -> Eligible JavaScript file found!!! Queueing for minification";
 					$js[] = $file;				
 					break;
 				default:
-					echo "<br/>File: ".$file." -> Not interested. Moving on.";
+					echo "\nFile: ".$file." -> Not interested. Moving on.";
 					break;
 			}
 		}
 		else {
-			echo "<br/>File: ".$file." -> Already Minified...";
+			echo "\nFile: ".$file." -> Already Minified...";
 		}
 		
 	}
 	if (count($css) || count($js)){
-		echo "<br/><br/>I have found ";
+		echo "\n\nI have found ";
 		if (count($css)){
 			echo count($css)." CSS files ";
 		}
@@ -66,47 +66,47 @@ function minifi($target){
 		if (count($js)){
 			echo count($js)." JavaScript files ";
 		}
-		echo "for minification.<br/><br/>";
+		echo "for minification.\n\n";
 		
 		echo "Beginning Minification...";
 		
 		if (count($css)){
-			echo "<br/>Minifying ".count($css)." CSS files:<br/>";
+			echo "\nMinifying ".count($css)." CSS files:\n";
 			
 			foreach ($css as $file){
 				$min = str_replace('css', 'min.css', $file);
 				$cmd = "curl -X POST -s --data-urlencode 'input@".$target."/".$file."' http://cssminifier.com/raw > ".$target."/".$min;
-				echo "<br/>Minifying ".$file."<br/>";			
+				echo "\nMinifying ".$file."\n";			
 				shell_exec($cmd);
-				echo "File minified and saved as ".$target."/".$min."<br/>";
+				echo "File minified and saved as ".$target."/".$min."\n";
 				
 			}
 		}
 		if (count($js)){
-			echo "<br/>Minifying ".count($js)." JavaScript files:<br/>";
+			echo "\nMinifying ".count($js)." JavaScript files:\n";
 			
 			foreach ($js as $file){
 				$min = str_replace('.js', '.min.js', $file);
 				$cmd = "curl -X POST -s --data-urlencode 'input@".$target."/".$file."' http://javascript-minifier.com/raw > ".$target."/".$min;
-				echo "<br/>Minifying ".$file."<br/>";			
+				echo "\nMinifying ".$file."\n";			
 				shell_exec($cmd);
-				echo "File minified and saved as ".$target."/".$min."<br/>";
+				echo "File minified and saved as ".$target."/".$min."\n";
 				
 			}
 		}
 
-		echo "<br/>Success!";
+		echo "\nSuccess!";
 		
 		if (count($css)){
-			echo "<br/>".count($css)." CSS files minified!";
+			echo "\n".count($css)." CSS files minified!";
 		}
 		if (count($js)){
-			echo "<br/>".count($js)." JavaScript files minified!";
+			echo "\n".count($js)." JavaScript files minified!";
 		}
 	}
 	else {
-		echo "<br/><br/>Awww... there's nothing to minify. Why'd you bother me?";
+		echo "\n\nAwww... there's nothing to minify. Why'd you bother me?";
 	}
-	echo "<br/><br/>";
+	echo "\n\n";
 }
 ?>
